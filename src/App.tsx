3b8e4b885e80e7bfb124271f60891e6449826abd
Miguel Magueijo <miguelmagueijo@hotmail.com>
2023-02-11 01:49:41 +0000

    Add React-Dropzone, colors and active style;
    Added react-dropzone and change drop zone in ap.tsx (not final);
    Added all league grey and gold colors;
    Rename color "leagueGolden" to "leagueGold";
    Rename color "leagueHextec" to "leagueHextecBlack"

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import LeagueOfLegends from "./routes/LeagueOfLegends/LeagueOfLegends";
 import RiotAccount from "./routes/RiotAccount/RiotAccount";
 import leagueLogo from "./assets/LoLGold.svg";
 import { useState } from "react";
+import { useDropzone } from "react-dropzone";
 
 export type GameTabs = {
 	Riot: JSX.Element,
@@ -25,24 +26,30 @@ const tabs: GameTabs = {
 }
 
 function App() {
-	const [currTab, setCurrTab] = useState<JSX.Element>(tabs.Riot);
+	const { acceptedFiles, getRootProps, getInputProps, open } = useDropzone({
+		accept: { "application/zip": [".zip"] },
+		noClick: true,
+		noKeyboard: true
+	});
+	const [ currTab, setCurrTab ] = useState<JSX.Element>(tabs.Riot);
 
 	return (
 		<div className="max-w-4xl mx-auto space-y-4 py-4">
 			<Navbar />
 			{/* Drop file area */}
-			<div id="dropArea" className="border-4 border-dashed border-white/70 rounded py-8 flex justify-center items-center space-y-4 flex-col bg-white/10">
+			<div id="dropArea" {...getRootProps({ className: "border-4 border-dashed border-white/70 rounded py-8 flex justify-center items-center space-y-4 flex-col bg-white/10" })}>
+				<input { ...getInputProps() } />
 				<h2 className="text-2xl font-bold">Drop your data here</h2>
-				<button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 duration-300 rounded font-bold">Choose file</button>
-				<span className="text-xs font-semibold text-white/50">Drop or choose .zip file</span>
+				<button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 duration-300 rounded font-bold" onClick={open}>Choose file</button>
+				{/* <h3 className="px-4 py-2 duration-300 rounded font-bold text-white/70">Click here to choose file</h3> */}
+				<span className="text-xs font-semibold text-white/50">Only supports .zip file</span>
 			</div>
 			{/* Tabs */}
 			<div>
 				<ul className="flex game-list">
 					<li onClick={() => { setCurrTab(tabs.Riot) }}>Riot Account</li>
 					{/* <li id="league">LoL</li> */}
-					<button></button>
-					<li id="league" onClick={() => { setCurrTab(tabs.League) }}>
+					<li id="league" className={ `${currTab === tabs.League ? "active" : ""} hover:text-leagueGold2 duration-300` }  onClick={() => { setCurrTab(tabs.League) }}>
 						<img src={leagueLogo} alt="League Of Legends Logo" />
 						<span className="block">League of Legends</span>
 					</li>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,10 +7,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        leagueHextec: "#010A13",
-        leagueGolden1: "#F0E6D2",
-        leagueGolden4: "#C89B3C",
+        leagueHextecBlack: "#010A13",
+        leagueGold1: "#F0E6D2",
+        leagueGold2: "#C8AA6E",
+        leagueGold3: "#C8AA6E",
+        leagueGold4: "#C89B3C",
+        leagueGold5: "#785A28",
+        leagueGold6: "#463714",
+        leagueGold7: "#32281E",
         leagueGrey1: "#A09B8C",
+        leagueGrey1_5: "#5B5A56",
+        leagueGrey2: "#3C3C41",
+        leagueGrey3: "#1E2328",
+        leagueGreyCool: "#1E282D"
+        // valoBackground: "#161928",
+        
       },
       fontFamily: {
         beaufortforlol: ["Beaufort For LoL"],
